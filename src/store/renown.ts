15d6570356c6e1daa87f9f6742998345bf27c61e
acaldas <acaldas.dev@gmail.com>
2023-08-23 09:45:47 +0100

    Default theme to light and save address to local storage

--- src/store/renown.ts
+++ src/store/renown.ts
@@ -1,5 +1,6 @@
 import { useAtom, useAtomValue } from 'jotai';
 import { atomWithStorage, createJSONStorage } from 'jotai/utils';
+import { useEffect } from 'react';
 import { getJWT as _getJWT, getAuth } from 'src/services/renown';
 import { getQueryParam } from 'src/utils/helpers';
 
@@ -17,6 +18,14 @@ export const jwtAtom = atomWithStorage<string | null>(
 );
 export const useGetJWT = () => {
     const [jwt, setJWT] = useAtom(jwtAtom);
+    const [, setAddress] = useAtom(addressAtom);
+
+    useEffect(() => {
+        const address = getQueryParam('address');
+        if (address) {
+            setAddress(address);
+        }
+    }, [window.location.search]);
 
     async function getJWT(
         address: string,
@@ -26,6 +35,7 @@ export const useGetJWT = () => {
         if (!jwt) {
             const newJWT = await _getJWT(address, publicKey, signChallenge);
             setJWT(newJWT);
+            setAddress(address);
             return newJWT;
         } else {
             // checks if the stored jwt corresponds to the current publicKey and address
