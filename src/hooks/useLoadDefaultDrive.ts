51c7e6c79fd5a214a3410e17176cc5124dbd5754
acaldas <acaldas.dev@gmail.com>
2024-06-18 17:15:14 +0100

    fea: added hack to force default drive reset

--- src/hooks/useLoadDefaultDrive.ts
+++ src/hooks/useLoadDefaultDrive.ts
@@ -1,17 +1,45 @@
 import { useEffect, useRef } from 'react';
 import { useDocumentDriveServer } from './useDocumentDriveServer';
 import { useFeatureFlag } from './useFeatureFlags';
+import defaultConfig from './useFeatureFlags/default-config';
 
 export const useLoadDefaultDrive = () => {
     const loading = useRef(false);
-    const { addRemoteDrive, documentDrives, documentDrivesStatus } = useDocumentDriveServer();
+    const {
+        addRemoteDrive,
+        documentDrives,
+        documentDrivesStatus,
+        clearStorage,
+    } = useDocumentDriveServer();
     const {
         setConfig,
         config: { defaultDrive },
     } = useFeatureFlag();
 
+    // TODO - remove this when we have a way to reset the default drive
+    async function resetDefaultDrive() {
+        console.log('Clearing default drive');
+        await clearStorage();
+        setConfig(defaultConfig);
+        loading.current = false;
+    }
+
     useEffect(() => {
-        if (defaultDrive && documentDrivesStatus === "LOADED" && !defaultDrive.loaded && !loading.current) {
+        if (
+            !loading.current &&
+            defaultDrive &&
+            defaultConfig.defaultDrive &&
+            defaultDrive.url !== defaultConfig.defaultDrive.url
+        ) {
+            loading.current = true;
+            void resetDefaultDrive();
+        }
+        if (
+            defaultDrive &&
+            documentDrivesStatus === 'LOADED' &&
+            !defaultDrive.loaded &&
+            !loading.current
+        ) {
             const isDriveAlreadyAdded = documentDrives.some(drive => {
                 return drive.state.local.triggers.some(
                     trigger => trigger.data?.url === defaultDrive.url,
